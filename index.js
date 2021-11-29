7a4379136b077ecdb1c831207913bacc99878a6d
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-11-29 06:24:19 -0500

    commit tree b1cdce3ef93fa7530bb9350255b1cc2224d76f76

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'albert';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'not albert';
+}
+
+
